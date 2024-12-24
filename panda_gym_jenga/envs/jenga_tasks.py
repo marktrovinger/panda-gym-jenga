1c7d7fcd2e2454cdb34d5e9092fdc91c7d790ccd
Mark <mark.trovinger@gmail.com>
2024-12-24 13:01:14 -0500

    started on deterministic for tower

--- panda_gym_jenga/envs/jenga_tasks.py
+++ panda_gym_jenga/envs/jenga_tasks.py
@@ -62,6 +62,121 @@ class JengaTowerEnv(RobotTaskEnv):
             render_roll
         )
 
+class JengaTowerDeterministicEnv(RobotTaskEnv):
+    """Pick and place task for Jenga blocks with the Panda robot.
+    Args:
+        render_mode (str, optional): Render mode. Defaults to "rgb_array".
+        reward_type (str, optional): "sparse" or "dense". Defaults to "sparse".
+        control_type (str, optional): "ee" to control end-effector position or "joints" to control joint values.
+            Defaults to "ee".
+        renderer (str, optional): Renderer, either "Tiny" or OpenGL". Defaults to "Tiny" if render mode is "human"
+            and "OpenGL" if render mode is "rgb_array". Only "OpenGL" is available for human render mode.
+        render_width (int, optional): Image width. Defaults to 720.
+        render_height (int, optional): Image height. Defaults to 480.
+        render_target_position (np.ndarray, optional): Camera targeting this position, as (x, y, z).
+            Defaults to [0., 0., 0.].
+        render_distance (float, optional): Distance of the camera. Defaults to 1.4.
+        render_yaw (float, optional): Yaw of the camera. Defaults to 45.
+        render_pitch (float, optional): Pitch of the camera. Defaults to -30.
+        render_roll (int, optional): Roll of the camera. Defaults to 0.
+        object_size (str, optional): Size of the Jenga block. Defaults to large (0.12 x 0.0381 x 0.0254) meters
+    """
+    def __init__(
+            self,
+            reward_type: str = "sparse",
+            control_type: str = "ee",
+            renderer: str = "Tiny",
+            render_mode: str ="rgb_array",
+            render_width = 720, 
+            render_height = 480, 
+            render_target_position = None, 
+            render_distance = 1.4, 
+            render_yaw = 45, 
+            render_pitch = -30, 
+            render_roll = 0,
+            object_size: str = "large"
+        ) -> None:
+        sim = PyBullet(render_mode=render_mode, renderer=renderer)
+        robot = Panda(sim, block_gripper=False, base_position=np.array([-0.6, 0.0, 0.0]), control_type=control_type)
+        task = JengaTower(sim, reward_type=reward_type, object_size=object_size)
+        super().__init__(
+            robot, 
+            task, 
+            render_width, 
+            render_height, 
+            render_target_position, 
+            render_distance, 
+            render_yaw, 
+            render_pitch, 
+            render_roll
+        )
+    def _robot_action(self, action):
+        # move to object
+        observation = self._get_obs()
+        current_position = observation["observation"][0:3]
+        # move to the object
+        if action == 0:
+            goal_position = observation["achieved_goal"][0:3]
+            if np.allclose(current_position, goal_position, self.task.distance_threshold):
+                self.is_action_completed = True
+                return current_position
+            else:
+                return 1.0 * (goal_position - current_position)
+        elif action == 1:
+            goal_position = observation["desired_goal"][0:3]
+            if np.allclose(current_position, goal_position, self.task.distance_threshold):
+                self.is_action_completed = True
+                return current_position
+            else: 
+                return 1.0 * (goal_position - current_position)
+        if action == 2:
+            self.robot.block_gripper = True
+            gripper_action = np.append(current_position, 0.0)
+            #print("Closing fingers")
+            self.is_action_completed = True
+            return gripper_action
+        else:
+            self.robot.block_gripper = False
+            gripper_action = np.append(current_position, 1.0)
+            self.is_action_completed = True
+            return gripper_action
+
+        
+
+    def step(self, action: np.ndarray):
+        # the actions need to be changed in order to work deterministically
+        reward = -1
+        goal_action = action
+        for i in range(1000):
+            # check to see if we are done with the overall objective
+            if self.is_action_completed and goal_action == 1:
+                observation = self._get_obs()
+                reward = reward * i
+                terminated = True
+                truncated = False
+                info = {"is_success": terminated,
+                        "time_taken": i
+                    }
+                self.is_action_completed = False
+                return observation, reward, terminated, truncated, info
+            elif self.is_action_completed and goal_action != 1:
+                observation = self._get_obs()
+                reward = reward * i
+                terminated = True
+                truncated = False
+                info = {"is_success": terminated,
+                        "time_taken": i
+                    }
+                self.is_action_completed = False
+                return observation, reward, terminated, truncated, info
+            action = self._robot_action(goal_action)
+            #dont forget to add the gripper
+            if goal_action < 2:
+                gripper = 0.0 if self.robot.get_fingers_width() else 1.0
+                action = np.append(action, gripper)
+            self.robot.set_action(action)
+            self.sim.step()
+
 
 class JengaPickAndPlaceEnv(RobotTaskEnv):
     """Pick and place task for Jenga blocks with the Panda robot.
