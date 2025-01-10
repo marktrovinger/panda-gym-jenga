b48151dd9e19b0326b7985c9e3eb2a0783f90c83
Mark Trovinger <mark.trovinger@gmail.com>
2025-01-10 11:22:06 -0500

    added entry for jenga 3 layer, updated ts budget

--- panda_gym_jenga/envs/__init__.py
+++ panda_gym_jenga/envs/__init__.py
@@ -6,5 +6,6 @@ from panda_gym_jenga.envs.jenga_tasks import (
     JengaSimplePickAndPlaceDeterministicEnv,
     JengaTowerEnv,
     JengaTowerDeterministicEnv,
-    JengaTower3DeterministicEnv
+    JengaTower3DeterministicEnv,
+    JengaTower3Env,
 )
