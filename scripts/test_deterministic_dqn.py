4da7e779645d2eea35f470874603ead8cbf6f792
Mark Trovinger <mark.trovinger@gmail.com>
2025-02-11 15:47:39 -0500

    working on q-learning

--- scripts/test_deterministic_dqn.py
+++ scripts/test_deterministic_dqn.py
@@ -2,7 +2,7 @@
 from stable_baselines3 import DQN, PPO
 import gymnasium as gym
 from rl_deterministic_actions.wrappers.deterministic_rl import DeterministicRLWrapper
-#from gymnasium.spaces import MultiDiscrete
+from gymnasium.spaces.utils import flatten_space
 import panda_gym_jenga
 import wandb
 import numpy as np
@@ -15,11 +15,22 @@ def main():
         "JengaTower3-v3", render_mode="human"
     )
     env = DeterministicRLWrapper(env)
-    #obs, _ = env.reset()
+    obs, _ = env.reset()
     #observation, reward, terminated, truncated, info = env.step(action+1)
+    obs_space = flatten_space(env.observation_space)
+    obs_space.shape
+    q = QAgent(flatten_space(env.observation_space).shape[0], flatten_space(env.action_space).shape[0])
 
-    model_dqn = DQN(policy="MlpPolicy", env=env)
-    model_dqn.learn(100)
+    for timestep in range(1000):
+        action = q.action(obs)
+        observation, reward, terminated, truncated, info = env.step(action)
+        print(f"Reward at timestep {timestep}: {reward}")
+        q.train(obs, action, reward, observation)
+        obs = observation
+
+
+    #model_dqn = DQN(policy="MlpPolicy", env=env)
+    #model_dqn.learn(100)
 
     #model = QAgent(state_space = env.observation_space, action_space=env.action_space)
     #model.train()
